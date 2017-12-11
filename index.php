0046e9d2357b8a614a1a898ab4cfe8dbbc473eb3
Chhavi Inani <ci38@afsconnect1.njit.edu>
2017-12-11 03:33:40 -0500

    added autoloader

--- index.php
+++ index.php
@@ -3,6 +3,15 @@
 ini_set('display_errors', 'On');
 error_reporting(E_ALL);
 
+//Class to load classes it finds the file when the program starts to fail for calling a missing class
+class Manage {
+    public static function autoload($class) {
+        include $class . '.php';
+    }
+}
+spl_autoload_register(array('Manage', 'autoload'));
+
+
 include_once'session.php';
 
 ?>
@@ -22,7 +31,7 @@ if(!isset($_SESSION['username'])):;
 ?>
 <p>You are not signed in <a href="login.php">Login</a> Not registered? <a href="signup.php">Signup</a></p>
 <?php else: ?>
-<p>You are logged in as <?php if(isset($_SESSION['username'])) echo $_SESSION['username'];?> <a href="logout.php">Logout</a></p>
+<p>You are logged in as <?php if(isset($_SESSION['username'])) echo $_SESSION['username'];?><br><br> <a href="list.php">Access your todo list </a><br><br><a href="logout.php">Logout</a></p>
 
 <?php endif  ?>
 
