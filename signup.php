aa78dadefc885aae4df199ecaba51f8f117e97c1
Chhavi Inani <ci38@afsconnect1.njit.edu>
2017-12-10 19:46:23 -0500

    added functions for code refactoring

--- signup.php
+++ signup.php
@@ -4,12 +4,12 @@ include_once 'database.php';
 include_once 'validation.php';
 
 //process the form
-if(isset($_POST['signupBtn'])){
+if(isset($_POST['signupBtn'])) {
 
     $form_errors = array();
 
     //Form validation
-    $required_fields = array('email', 'username','fname', 'lname', 'password');
+    $required_fields = array('email', 'username', 'fname', 'lname', 'password');
 
     $form_errors = array_merge($form_errors, check_empty_fields($required_fields));
 
@@ -19,41 +19,40 @@ if(isset($_POST['signupBtn'])){
 
     $form_errors = array_merge($form_errors, check_email($_POST));
 
-    if(empty($form_errors)){
-        //collect form data and store in variables
-        $email = $_POST['email'];
-        $username = $_POST['username'];
-        $fname = $_POST['fname'];
-        $lname = $_POST['lname'];
-        $password = $_POST['password'];
+    //collect form data and store in variables
+    if (empty($form_errors)) {
+    $email = $_POST['email'];
+    $username = $_POST['username'];
+    $fname = $_POST['fname'];
+    $lname = $_POST['lname'];
+    $password = $_POST['password'];
 
-        //hashing the password
-        $hashed_password = password_hash($password, PASSWORD_BCRYPT);
-        try{
+    //hashing the password
+    $hashed_password = password_hash($password, PASSWORD_BCRYPT);
+    try {
 
-            $sql = "INSERT INTO accounts (email, username, fname, lname, password) VALUES (:email, :username, :fname, :lname, :password)";
-            $statement = $db->prepare($sql);
-            $statement->execute(array(':email' => $email,':username' => $username, ':fname' => $fname, ':lname' => $lname,':password' => $hashed_password));
+        $sql = "INSERT INTO accounts (email, username, fname, lname, password) VALUES (:email, :username, :fname, :lname, :password)";
+        $statement = $db->prepare($sql);
+        $statement->execute(array(':email' => $email, ':username' => $username, ':fname' => $fname, ':lname' => $lname, ':password' => $hashed_password));
 
-            //check if one new row was created
-            if($statement->rowCount() == 1){
-                $result = "<p style='padding:20px; border: 1px solid gray; color: green;'> Registration Successful</p>";
-            }
-        }catch (PDOException $ex){
-            $result = "<p style='padding:20px; border: 1px solid gray; color: red;'> An error occurred: ".$ex->getMessage()."</p>";
+        //check if one new row was created
+        if ($statement->rowCount() == 1) {
+            $result = flashMessage("Registration Successful", "Pass");
+        }
+    } catch (PDOException $e) {
+        $result = flashMessgae("An error occurred: " . $e->getMessage());
+    }
+}else {
+        if (count($form_errors) == 1) {
+            $result = flashMessgae("There was 1 error in the form<br>");
+        } else {
+            $result = flashMessage("There were " . count($form_errors) . " errors in the form <br>");
         }
     }
-    else{
-        if(count($form_errors) == 1){
-            $result = "<p style='color: red;'> There was 1 error in the form<br>";
-        }else{
-            $result = "<p style='color: red;'> There were " .count($form_errors). " errors in the form <br>";
-        }
-    }
-
 }
-
 ?>
+
+
 <!DOCTYPE html>
 <html>
 <head lang="en">
