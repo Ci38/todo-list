5a7330fedd46a4ce36ff184d8f878794956a18f2
Chhavi Inani <ci38@afsconnect1.njit.edu>
2017-12-12 22:11:03 -0500

    addded css

--- login.php
+++ login.php
@@ -2,7 +2,6 @@
 include_once 'session.php';
 include_once 'database.php';
 include_once 'validation.php';
-include_once 'parseLogin.php';
 
 if(isset($_POST['LoginBtn'])){
 
@@ -54,19 +53,82 @@ if(isset($_POST['LoginBtn'])){
     <title>Login Page</title>
 </head>
 <body>
-<h2>Todo List Application</h2><hr>
 
-<h3>Login Form</h3>
+<style>
+
+    .center {
+        text-align: center;
+    }
+
+    input[type=text], select {
+        width: 40%;
+        padding: 8px 20px;
+        margin: 8px 0;
+        display: table;
+        border: 1px solid #ccc;
+        border-radius: 4px;
+        box-sizing: border-box;
+    }
+
+    input[type=password], select {
+        width: 40%;
+        padding: 8px 20px;
+        margin: 8px 0;
+        display: table;
+        border: 1px solid #ccc;
+        border-radius: 4px;
+        box-sizing: border-box;
+    }
+
+
+    input[type=submit] {
+        width: 40%;
+        background-color: #4CAF50;
+        color: white;
+        padding: 10px 10px;
+        margin: 8px 0;
+        border: none;
+        border-radius: 4px;
+        cursor: pointer;
+    }
+
+    input[type=submit]:hover {
+        background-color: #45a049;
+    }
+
+    div {
+        border-radius: 5px;
+        background-color: #f2f2f2;
+        padding: 10px;
+    }
+
+    p.serif {
+        font-family: "Comic Sans MS";
+    }
+
+</style>
+<body>
+
+<h2><center><p class="serif">Todo List Application</p></center></h2>
+
+<h3><center>Login Form</center></h3>
 
 <?php if(isset($result)) echo $result; ?>
 <?php if(!empty($form_errors)) echo show_errors($form_errors); ?>
-<form method="post" action="">
-    <table>
-        <tr><td>Username:</td> <td><input type="text" value="" name="username"></td></tr>
-        <tr><td>Password:</td> <td><input type="password" value="" name="password"></td></tr>
-        <tr><td></td><td><input style="float: right;" type="submit" name="LoginBtn" value="Sign-in"></td></tr>
-    </table>
-</form>
+<div>
+
+    <form method="post" action="">
+
+        <label for="username">UserName</label>
+        <input type="text" value="" name="username" placeholder="Your UserName..">
+
+        <label for="password">Password</label>
+        <input type="password" value="" name="password" placeholder="Your Password..">
+
+
+        <input type="submit" name="LoginBtn" value="Sign-In">
+    </form>
+</div>
 <p><a href="index.php">Back</a> </p>
 </body>
 </html>
