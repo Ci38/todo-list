3cc4c263294763ccf2ed33d3397033020222b412
Chhavi Inani <ci38@afsconnect1.njit.edu>
2017-12-12 22:09:14 -0500

    added css

---
+++ csspage.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <style>
+        * {
+            box-sizing: border-box;
+        }
+
+        body {
+            margin: 0;
+        }
+
+        .header {
+            background-color: #f1f1f1;
+            padding: 2px;
+            text-align: center;
+        }
+
+        .topnav {
+            overflow: hidden;
+            background-color: #333;
+        }
+
+        .topnav a {
+            float: left;
+            display: block;
+            color: #f2f2f2;
+            text-align: center;
+            padding: 14px 16px;
+            text-decoration: none;
+        }
+
+        .topnav a:hover {
+            background-color: #ddd;
+            color: black;
+        }
+
+
+        .row:after {
+            content: "";
+            display: table;
+            clear: both;
+        }
+
+        p.serif {
+            font-family: "Comic Sans MS";
+        }
+    </style>
+
+
+</head>
+<body>
+
+<div class="header">
+    <h1><p class="serif">Todo List Application </p></h2></h1>
+    <p></p>
+</div>
+<div class="row">
+</div>
+</body>
+</html>
