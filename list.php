9b23cc7c34444f60261d47bb65fec774178fc84d
Chhavi Inani <ci38@afsconnect1.njit.edu>
2017-12-17 01:36:30 -0500

    committed file

--- list.php
+++ list.php
@@ -1,74 +1,46 @@
 <?php
+include_once 'session.php';
+include_once 'csspage.php';
 
-include_once 'database.php';
-include_once 'validation.php';
-
-
-session_start();
+if (isset($_SESSION['name'], $_SESSION['user_id'], $_SESSION['isLogged'])) {
+?>
+<?php
+global $result;
 ?>
 
-<html>
-<title> Todo List </title>
-<body>
-<br>
-<p><a href="index.php">Back</a><br><br><a href="logout.php">Logout</a></p>
-<p>You are logged-in as <?php if(isset($_SESSION['username'])) echo $_SESSION['username'];?><br></p><hr>
-<div class="container">
-    <div class="jumbotron">
-        <?php
-        echo "<h1> Your Todo List </h1><br/>";
-        $user=$_SESSION["user"];
-        echo "Welcome, " . $_SESSION["username"] . '<br/>';
-        echo "Your todo items are displayed below ";
-        echo "<br> <br>";
-        ?>
-    </div>
-    <div class="col-md-2"></div>
-    <div class="col-md-8">
-        <div class="table-responsive">
-            <table class="table table-striped">
-                <thead>
-                <tr>
-                    <th>Todo</th>
+<div>
+    <h3 class="center"> Todo List </h3>
 
-                </tr>
-                </thead>
-                <tbody>
-                <?php  foreach ($result as $res): ?>
-                    <tr>
+    <?php
+    if (!empty($result)){
 
-                        <td> <?php echo $res['todo_item']; ?>  </td>
-                        <td> <?php echo $res['Due_Date'];?> </td>
-                        <td> <form method = 'post' action='edit_item.php'>
+        foreach ($result as $res1):
+            ?>
+            <?php echo $res1['todo_title'] ?>
+            <br>
 
-                                <input type="hidden" value="<?php echo $res['id'];?> " name="task_id"/>
-                                <input type="hidden" value="<?php echo $res['todo_item'];?> " name="task_name"/>
-                                <input type="hidden" value="<?php echo $res['Due_Date'];?> " name="ddate"/>
-                                <input type="submit" class="btn btn-info" value="Edit"/> </form> </td>
+            <form action="editTodo.php" method="post" class="edit_todo_form">
+                <input type="hidden" name="todo_id" value="<?php echo $res1['id'] ?>">
+                <button type="submit"><b>View / Edit Todo</b></button>
+            </form>
+            <br>
+            <form action="index.php" method="post" class="delete_todo_form">
+                <input type="hidden" name="todo_id" value="<?php echo $res1['id'] ?>">
+                <input type="hidden" value="delete" name="action">
+                <button type="submit"><b>Delete Todo</b></button>
+            </form>
 
+            <?php
+        endforeach;
+    } else {
+        echo '<h3 class="center grey-text">Add todos to get started</h3>';
+    } ?>
+    <br>
+    <a href="addTodo.php"><button class="button" style="margin-bottom: 15px;"><b><h4>Add Todo</h4></b></button></a>
 
-                        <td> <form method = 'post' action='index.php'>
-                                <input type="hidden" name="action" value="complete">
-                                <input type="hidden" value="<?php echo $res['id']; ?> " name="task_id" />
-                                <input type="submit" class="btn btn-success" value="Complete"/> </form> </td>
+    <?php
+    } else {
+        header("Location: index.php");
+    }
+    ?>
 
-                        <td> <form method = 'post' action='index.php'>
-                                <input type="hidden" name="action" value="delete">
-                                <input type="hidden" value="<?php echo $res['id']; ?> "  name="task_id"/>
-                                <input type="submit" class="btn btn-danger" value="Delete"/> </form> </td>
-
-                    </tr>
-                    <?php
-                endforeach;
-                ?>
-                <td> <form method = 'post' action='add_todoitem.php'>   <input class="btn btn-primary" type="submit" value="Add Todo"/> </form></td>
-                <td> <form method = 'post' action='edit_item.php'>   <input type="hidden" name="action" value="compitems"> <input class="btn btn-success" type="submit" value="Edit Todo"/> </form></td>
-
-                <td> <form method = 'post' action='index.php'>   <input type="hidden" name="action" value="pend"> <input class="btn btn-warning" type="submit" value="Delete Todo"/> </form></td>
-                </tbody>
-        </div>
-        <div class="col-md-2"></div>
-    </div>
-</div>
-</body>
-</html>
