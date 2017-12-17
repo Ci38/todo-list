21e867cdbbcd320e83900c037c42448260baeaca
Chhavi Inani <ci38@afsconnect1.njit.edu>
2017-12-17 01:36:56 -0500

    committed

--- login.php
+++ login.php
@@ -1,49 +1,9 @@
 <?php
+include_once 'csspage.php';
+include_once 'footer.php';
 include_once 'session.php';
-include_once 'database.php';
-include_once 'validation.php';
 
-if(isset($_POST['LoginBtn'])){
-
-    $form_errors = array();
-
-//validate
-    $required_fields = array('username', 'password');
-    $form_errors = array_merge($form_errors, check_empty_fields($required_fields));
-
-    if(empty($form_errors)){
-
-        //collect form data
-        $user = $_POST['username'];
-        $password = $_POST['password'];
-
-        //check if user exist in the database
-        $query = "SELECT * FROM accounts WHERE username = :username";
-        $statement = $db->prepare($query);
-        $statement->execute(array(':username' => $user));
-
-        while($row = $statement->fetch()){
-            $id = $row['id'];
-            $hashed_password = $row['password'];
-            $username = $row['username'];
-
-            if(password_verify($password, $hashed_password)){
-                $_SESSION['id'] = $id;
-                $_SESSION['username'] = $username;
-                redirectto('index');
-            }else{
-                $result = flashMessage("Invalid username or password");
-            }
-        }
-
-    }else{
-        if(count($form_errors) == 1){
-            $result = flashMessage("There was an error in the form");
-        }else{
-            $result = flashMessage("There were " .count($form_errors). " errors in the form");
-        }
-    }
-}
+if (!isset($_SESSION['name'], $_SESSION['user_id'], $_SESSION['isLogged'])) {
 ?>
 
 <!DOCTYPE html>
@@ -60,7 +20,7 @@ if(isset($_POST['LoginBtn'])){
         text-align: center;
     }
 
-    input[type=text], select {
+    input[type=email], select {
         width: 40%;
         padding: 8px 20px;
         margin: 8px 0;
@@ -83,7 +43,7 @@ if(isset($_POST['LoginBtn'])){
 
     input[type=submit] {
         width: 40%;
-        background-color: #4CAF50;
+        background-color: #5160d1;
         color: white;
         padding: 10px 10px;
         margin: 8px 0;
@@ -93,13 +53,7 @@ if(isset($_POST['LoginBtn'])){
     }
 
     input[type=submit]:hover {
-        background-color: #45a049;
-    }
-
-    div {
-        border-radius: 5px;
-        background-color: #f2f2f2;
-        padding: 10px;
+        background-color: #0000FF;
     }
 
     p.serif {
@@ -107,28 +61,33 @@ if(isset($_POST['LoginBtn'])){
     }
 
 </style>
-<body>
-
-<h2><center><p class="serif">Todo List Application</p></center></h2>
-
-<h3><center>Login Form</center></h3>
-
-<?php if(isset($result)) echo $result; ?>
-<?php if(!empty($form_errors)) echo show_errors($form_errors); ?>
-<div>
-
-    <form method="post" action="">
-
-        <label for="username">UserName</label>
-        <input type="text" value="" name="username" placeholder="Your UserName..">
-
-        <label for="password">Password</label>
-        <input type="password" value="" name="password" placeholder="Your Password..">
 
 
-        <input type="submit" name="LoginBtn" value="Sign-In">
-    </form>
-</div>
-<p><a href="index.php">Back</a> </p>
+<h3><center>Login Form </center></h3>
+
+<form method="POST" id="login_center_form" action="index.php">
+
+    <label for="email">Email</label>
+    <input class="validate" type="email" name="email" placeholder="Your Email" required>
+
+    <br>
+
+    <label for="password">Password</label>
+    <input class="validate" type="password"  name="password" placeholder="Your password" required>
+
+    <input type="hidden" name="action" value="checking_user">
+    <br>
+    <button class="button" type="submit">Login</button><br>
+    <br><br>
+    &nbsp;&nbsp;Not Registered? <a href="signup.php">Sign Up</a>
+</form>
+<div id="validate_login_check"></div>
+
+<?php
+} else {
+    echo 'You are logged in <a href="index.php">Back?</a>';
+}
+
+?>
 </body>
 </html>
