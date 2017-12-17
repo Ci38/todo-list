465f323e9deb77ac9b28ad3411ef5b9a5fe2dfe1
Chhavi Inani <ci38@afsconnect1.njit.edu>
2017-12-17 01:19:17 -0500

    committed this file

--- csspage.php
+++ csspage.php
@@ -1,3 +1,7 @@
+<?php
+include_once 'session.php';
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -13,14 +17,14 @@
         }
 
         .header {
-            background-color: #f1f1f1;
+            background-color: #d4d8f7;
             padding: 2px;
             text-align: center;
         }
 
         .topnav {
             overflow: hidden;
-            background-color: #333;
+            background-color: #5160d1;
         }
 
         .topnav a {
@@ -47,17 +51,53 @@
         p.serif {
             font-family: "Comic Sans MS";
         }
+        .button {
+            background-color: #008CBA;
+            border: none;
+            color: white;
+            padding: 15px 32px;
+            text-align: center;
+            text-decoration: none;
+            display: inline-block;
+            font-size: 16px;
+            margin: 4px 2px;
+            cursor: pointer;
+        }
+
     </style>
 
 
 </head>
 <body>
 
-<div class="header">
-    <h1><p class="serif">Todo List Application </p></h2></h1>
-    <p></p>
-</div>
-<div class="row">
-</div>
+
+<header>
+    <head lang="en">
+        <meta charset="UTF-8">
+    </head>
+    <div class="header">
+        <h1><p class="serif"><font color="#1b2cad">Todo List Application </font></p></h2></h1>
+        <p></p>
+    </div>
+
+    <?php
+    if (isset($_SESSION['name'], $_SESSION['user_id'], $_SESSION['isLogged'])):;
+        ?>
+
+        <div class="topnav">
+            <a href="profile.php">My Profile </a>
+            <a href="logout.php">Logout</a>
+
+        </div>
+
+        <h2><p><center>Hello, <?php echo $_SESSION["name"];?>!</center></p></h2>
+
+    <?php else: ?>
+
+    </div>
+    <h3><center>This is a simple to do list application based on PHP.</center></h3><br><br><br>
+    <h3><center> Please <a href="login.php"> Login</a> or <a href="signup.php"> Sign Up</a> to get started.</center></h3>
+</header>
+<?php endif ?>
 </body>
 </html>
